6913cddd11fc9acc590ac53a1fb4d1c84f11fd5d
wanglei <2365104338@qq.com>
2020-12-11 19:12:04 +0800

    fix bug 样式冲突的问题

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -6,34 +6,34 @@
       <img class="img" src="../assets/smile.svg">
       <span>免冠照</span>
     </div>
-    <div>
+    <div class="item">
       <img class="img" src="../assets/add-account.svg">
-      <XInput class="input" title="拜访对象" placeholder="请输入拜访人名称">
+      <XInput class="input" title="拜访对象" placeholder="请输入拜访人名称" :show-clear="false">
       </XInput>
     </div>
     <div class="item">
       <img class="img" src="../assets/company.svg">
-      <XInput class="input" title="拜访单位" placeholder="请输入拜访单位名称">
+      <XInput class="input" title="拜访单位" placeholder="请输入拜访单位名称" :show-clear="false">
       </XInput>
     </div>
     <div class="item">
       <img class="img" src="../assets/company.svg">
-      <XInput class="input" title="您的姓名" placeholder="请输入您的姓名">
+      <XInput class="input" title="您的姓名" placeholder="请输入您的姓名" :show-clear="false">
       </XInput>
     </div>
     <div class="item">
       <img class="img" src="../assets/company.svg">
-      <XInput class="input" title="手机号码" placeholder="请输入您的手机号码">
+      <XInput class="input" title="手机号码" placeholder="请输入您的手机号码" :show-clear="false">
       </XInput>
     </div>
     <div class="item">
       <img class="img" src="../assets/company.svg">
-      <XInput class="input" title="您的单位" placeholder="请输入您的单位">
+      <XInput class="input" title="您的单位" placeholder="请输入您的单位" :show-clear="false">
       </XInput>
     </div>
     <div class="item">
       <img class="img" src="../assets/company.svg">
-      <XInput class="input" title="车牌号码" placeholder="请输入您的车牌号码">
+      <XInput class="input" title="车牌号码" placeholder="请输入您的车牌号码" :show-clear="false">
       </XInput>
     </div>
     <div class="item">
@@ -72,13 +72,7 @@ export default {
   margin-top: 20px;
 }
 
-.weui-icon-filled-add-friends {
-  color: #1890FF;
-  margin-left: 30px
-}
-
 .item {
-
 }
 
 .img {
